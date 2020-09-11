9a0ee8cb8043f04c4a6f0aaf27127f1d57576cbb
tycrek <josh.moore@jmoore.dev>
2020-09-11 11:58:11 -0600

    Added about

--- modules/info.js
+++ modules/info.js
@@ -35,4 +35,14 @@ module.exports = {
 			.setURL('https://github.com/tycrek/mandatum')
 			.setFooter('Check out my source code on GitHub!'))
 			.catch((err) => log.warn(err)),
+
+	about: (msg) =>
+		msg.channel.send(new MessageEmbed({
+			"title": "Hello! :wave:",
+			"description": "I'm a Discord bot. You can check out my **[source code](https://github.com/tycrek/mandatum/)**.\n\n**What does \"mandatum\" mean?** It's Latin for \"command\".\n\nRun `>help` to see a list of commands.\n",
+			"color": 16776960,
+			"footer": "Created by tycrek",
+			"thumbnail": "https://cdn.discordapp.com/avatars/750806884914692207/d38112a55f14509e68e9823871ecf2eb.png?size=4096"
+		})
+			.setThumbnail('https://cdn.discordapp.com/avatars/750806884914692207/d38112a55f14509e68e9823871ecf2eb.png?size=4096'))
 }
