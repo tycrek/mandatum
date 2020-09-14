4053b8b0591f96b36ee5fd0db5f31080a3aff156
tycrek <josh.moore@jmoore.dev>
2020-09-14 10:05:37 -0600

    squished UsageEmbed

--- UsageEmbed.js
+++ UsageEmbed.js
@@ -7,6 +7,7 @@
 const { MessageEmbed } = require('discord.js');
 
 const paramLead = '> '; // Displayed before the parameter help line. This is UNRELATED to prefix; > has best results
+const newLine = '\n\n';
 
 class UsageEmbed extends MessageEmbed {
 	/**
@@ -16,26 +17,28 @@ class UsageEmbed extends MessageEmbed {
 	 * @param {boolean} wrap Place the separator on both sides of the parameter (useful for quotations as separators)
 	 * @param {string[]} parameters Parameters sent to the command
 	 * @param {string[]} descriptions Descriptions of each parameter
-	 * @param {string[]} notes Notes/tips on using the command
+	 * @param {string[]} notes Notes/tips on using the command (can be null)
 	 */
 	constructor(command, separator, wrap, parameters, descriptions, notes = null) {
 		let prefix = require('./bot').prefix; // * Copy/pasting? This line may be different for you
-
-		//! STEP 1: First line
-		let usageLine = `\`${prefix}${command} ${wrap ? separator.split('').shift() : ''}${parameters.join(separator)}${wrap ? separator.split('').pop() : ''}\``;
-
-		//! STEP 2: Parameter text
-		let parameterLines = parameters.map((param) =>
-			`\`${(paramLead + param).padEnd(parameters.reduce((a, b) =>
-				a.length > b.length ? a : b).length + paramLead.length + (' '.length * 4))}\` ${descriptions[parameters.indexOf(param)]}`).join('\n');
-
-		//! STEP 3: Notes
-		let noteLines = notes && notes.length !== 0 ? `Note: ${notes.join('; ')}` : '';
-
-		//! STEP 4: Pass this all back to Discord.js MessageEmbed
 		super({
-			title: `${prefix}${command} Usage`,
-			description: usageLine + '\n\n' + parameterLines + '\n\n' + noteLines
+			title: `Usage`,
+			description: (
+				//! STEP 1: First line
+				(`\`${prefix}${command} ${wrap ? separator.split('').shift() : ''}${parameters.join(separator)}${wrap ? separator.split('').pop() : ''}\``)
+
+				+ newLine +
+
+				//! STEP 2: Parameter text
+				(['**Parameters**'].concat(parameters.map((param) =>
+					`\`${(paramLead + param).padEnd(parameters.reduce((a, b) =>
+						a.length > b.length ? a : b).length + paramLead.length + (' '.length * 4))}\` ${descriptions[parameters.indexOf(param)]}`)).join('\n'))
+
+				+ newLine +
+
+				//! STEP 3: Notes
+				(notes && notes.length !== 0 ? `Note: ${notes.join('; ')}` : '')
+			)
 		});
 	}
 }
