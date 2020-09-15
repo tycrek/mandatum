bcfbe68d023a24078adf8c1a802e95948fd04414
tycrek <josh.moore@jmoore.dev>
2020-09-15 13:06:30 -0600

    Major improvements to vote

--- modules/moderator.js
+++ modules/moderator.js
@@ -191,12 +191,60 @@ module.exports = {
 	},
 
 	vote: (msg) => {
+		const emoji = {
+			up: '👍',
+			down: '👎'
+		};
+
 		const args = msg.content.slice(prefix.length).trim().split(/ +/);
 		args.shift(); // Remove command from args
 
-		msg.channel.send(new MessageEmbed()
-			.setTitle('Vote now!')
-			.setDescription(args.join(' ')))
-			.then((msg) => Promise.all([msg.react('👍'), msg.react('👎')]));
-	}
+		// According to JavaScript, "Not a Number" is typeof number........
+		if (args.length < 2 || parseInt(args[0]).toString() === 'NaN')
+			return msg.channel.send(new UsageEmbed('vote', ' ', false, ['time', 'topic'], ['time in seconds', 'what users will vote on'], ['topic can be multiple words']));
+
+		// General information about the vote
+		let time = parseInt(args.shift());
+		let topic = args.join(' ');
+		let author = msg.member.displayName;
+
+		// This is set and dealt with later
+		let reactMsg;
+		msg.channel.send(
+			new MessageEmbed()
+				.setAuthor('Vote now!')
+				.setTitle(`${topic}${!topic.endsWith('?') ? '?' : ''}`)
+				.setFooter(`You have ${time} seconds to vote!\nVote ends at ${moment().add(time, 's').format('h:mm:ss a, MMMM Do, YYYY')} UTC`))
+			.then((mReactMsg) => reactMsg = mReactMsg)
+
+			// Wait for both reactions to appear before continuing (we get rate limited really easily with reactions)
+			.then(() => Promise.all([reactMsg.react(emoji.up), reactMsg.react(emoji.down)]))
+
+			// Listen for reactions for time in milliseconds
+			.then((_reactions) => reactMsg.awaitReactions((reaction) => reaction.emoji.name === emoji.up || reaction.emoji.name === emoji.down, { time: time * 1000 }))
+
+			// Process the collected reactions
+			.then((collected) => {
+				let votes = {
+					[emoji.up]: 0,
+					[emoji.down]: 0
+				};
+
+				// Go through the reactions (if no one clicks a reaction, it will not show up in collected. Subtract 1 to not count the bot)
+				collected.each((col) => votes[col._emoji.name === emoji.up ? emoji.up : emoji.down] = col.count - 1);
+
+				return votes;
+			})
+			.then((votes) =>
+				reactMsg.edit(
+					new MessageEmbed()
+						.setAuthor('Votes are in!')
+						.setTitle(`${topic}${!topic.endsWith('?') ? '?' : ''}`)
+						.setDescription(`${emoji.up} : ${votes[emoji.up]}\u2003\u2003${emoji.down} : ${votes[emoji.down]}`)
+						.setFooter(`Vote requested by ${author}\nVote Concluded at ${moment().format('h:mm:ss a, MMMM Do, YYYY')} UTC`)))
+
+			// Remove the emoji after vote completes
+			.then((editedMsg) => editedMsg.reactions.removeAll())
+			.catch((err) => log.error(err));
+	},
 }
