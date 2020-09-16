f43bd14ac0e5dfaf2fa33d4d8e916553bf53301d
tycrek <josh.moore@jmoore.dev>
2020-09-16 09:56:00 -0600

    Added channel stats

--- bot.js
+++ bot.js
@@ -91,6 +91,39 @@ client.once('ready', () => {
 			});
 	});
 
+	// Update members if needed
+	client.guilds.cache.each((guild) => {
+		let configPath = path.join(__dirname, `config/servers/guild.${guild.id}.json`);
+		fs.exists(configPath)
+			.then((exists) => {
+				if (!exists) throw Error('');
+				else return fs.readJson(configPath);
+			})
+			.then((config) => {
+				if (!config.stats) throw Error(`No stats for guild [${guild.id}], ignoring`);
+				else return Promise.all([
+					client.guilds.resolve(guild.id).members.cache,
+					client.guilds.resolve(guild.id).channels.resolve(config.stats.members),
+					client.guilds.resolve(guild.id).channels.resolve(config.stats.bots)
+				]);
+			})
+			.then((results) => {
+				let members = bots = 0;
+				results[0].each((member) => member.user.bot ? bots++ : members++);
+
+				let oldMembersPrefix = results[1].name.split(' ')[0];
+				let oldMembersSuffix = parseInt(results[1].name.split(' ')[1]);
+				let oldBotsPrefix = results[2].name.split(' ')[0];
+				let oldBotsSuffix = parseInt(results[2].name.split(' ')[1]);
+
+				let newMembers = `${oldMembersPrefix} ${members}`;
+				let newBots = `${oldBotsPrefix} ${bots}`;
+
+				return Promise.all([results[1].setName(newMembers), results[2].setName(newBots)]);
+			})
+			.catch((err) => log.warn(err.message));
+	});
+
 	// Custom status
 	client.user.setActivity(`the world burn (${prefix})`, { type: "WATCHING" })
 		.catch((err) => log.warn(err));
@@ -112,6 +145,30 @@ client.on('warn', (warn) => log.warn(warn));
 client.on('error', (error) => log.error(error));
 client.on('invalidated', () => log.fatal('Session invalidated, please restart!'));
 client.on('rateLimit', (info) => log.warn(`API rate limit with ${info.timeout}ms delay!`));
+
+var statsUpdate = (member, op) => {
+
+	let guild = member.guild.id;
+	let configPath = path.join(__dirname, `./config/servers/guild.${guild}.json`);
+	fs.readJson(configPath)
+		.then((config) => {
+			if (!config.stats) throw Error(`No stats for guild [${guild}], ignoring`);
+			else {
+				let channel = member.guild.channels.resolve(config.stats[member.user.bot ? 'bots' : 'members']);
+
+				let prefix = channel.name.split(' ')[0];
+				let suffix = parseInt(channel.name.split(' ')[1]);
+
+				let newName = `${prefix} ${op === 0 ? suffix += 1 : suffix -= 1}`;
+
+				return channel.setName(newName);
+			}
+		})
+		.catch((err) => log.warn(err));
+}
+
+client.on('guildMemberAdd', (member) => statsUpdate(member, 0));
+client.on('guildMemberRemove', (member) => statsUpdate(member, 1));
 
 // Command processor
 client.on('message', (msg) => {
--- modules/admin.js
+++ modules/admin.js
@@ -134,5 +134,56 @@ module.exports = {
 			.then(() => msg.member.createDM())
 			.then((channel) => channel.send(`**${count}** messages created!`))
 			.catch((err) => log.warn(err));
+	},
+
+	stats: (msg) => {
+		const args = msg.content.slice(prefix.length).trim().split(/ +/);
+		let command = args.shift();
+		let category = args.join('-');
+
+		let age = msg.guild.createdTimestamp;
+		let everyone = msg.guild.roles.everyone.id;
+		let configPath = path.join(__dirname, `../config/servers/guild.${msg.guild.id}.json`);
+
+		let members = bots = 0;
+
+		msg.guild.members.cache.each((member) => member.user.bot ? bots++ : members++);
+
+		msg.guild.channels.create(category, { type: 'category' })
+			.then((c) => c.setPosition(0))
+			.then((c) => Promise.all([
+				fs.readJson(configPath),
+				c.id,
+				c.guild.channels.create(`Members: ${members}`, { type: 'voice', parent: c.id, permissionOverwrites: [{ id: everyone, deny: 1048576 }, { id: require('../bot').client.user.id, allow: 1048592 }] }),
+				c.guild.channels.create(`Bots: ${bots}`, { type: 'voice', parent: c.id, permissionOverwrites: [{ id: everyone, deny: 1048576 }, { id: require('../bot').client.user.id, allow: 1048592 }] })
+			]))
+			.then((results) => {
+				let config = results[0];
+				config.stats = {
+					parent: results[1],
+					members: results[2].id,
+					bots: results[3].id
+				};
+				return config;
+			})
+			.then((config) => fs.writeJson(configPath, config, { spaces: '\t' }))
+			.then(() => msg.channel.send('Stats channels created successfully.'))
+			.then((botMsg) => trash(msg, botMsg))
+			.catch((err) => log.warn(err));
+	},
+
+	delstats: (msg) => {
+		let configPath = path.join(__dirname, `../config/servers/guild.${msg.guild.id}.json`);
+
+		let config;
+		fs.readJson(configPath)
+			.then((mConfig) => config = mConfig)
+			.then(() => { if (!config.stats) throw Error('No stats data in config') })
+			.then(() => msg.guild.channels)
+			.then((channels) => Promise.all([channels.resolve(config.stats.parent), channels.resolve(config.stats.members), channels.resolve(config.stats.bots)]))
+			.then((stats) => Promise.all(stats.map((statChannel) => statChannel.delete())))
+			.then((_results) => msg.channel.send('Deleted stats channels'))
+			.then((botMsg) => trash(msg, botMsg))
+			.catch((err) => log.warn(err));
 	}
 }
