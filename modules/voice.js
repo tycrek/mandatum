7408ac20820a8f2f95c663940d5a60d9f2253d13
tycrek <josh.moore@jmoore.dev>
2020-09-22 15:33:37 -0600

    really broken rn

--- modules/voice.js
+++ modules/voice.js
@@ -1,8 +1,17 @@
+const { MessageEmbed } = require('discord.js');
 const ytdl = require('ytdl-core');
 const { log, trash, filter, noPermission } = require('../utils');
 const { prefix, owner, client } = require('../bot');
 const UsageEmbed = require('../UsageEmbed');
 const fetch = require('node-fetch');
+const { google } = require('googleapis');
+
+const YT = google.youtube({
+	version: 'v3',
+	auth: require('../auth.json').youtube
+});
+var queue = {};
+var controls = {};
 
 module.exports = {
 	vjoin: (msg) => {
@@ -15,7 +24,6 @@ module.exports = {
 
 		// Connect to users channel
 		msg.member.voice.channel.join()
-			//.then((connection) => connection.play(args[1].includes('youtube') ? ytdl(args[1]) : args[1]))
 			.then(() => msg.channel.send('Connected!'))
 			.then((botMsg) => trash(msg, botMsg))
 			.catch((err) => log.warn(err));
@@ -37,13 +45,73 @@ module.exports = {
 			.catch((err) => log.warn(err));
 	},
 
+	vsearch: (msg) => {
+		const emoji = ['1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣'];
+
+		let vc = getVoice(msg);
+
+		if (!isMemberVoice(msg))
+			return msg.reply('Please join a voice channel first!').then((botMsg) => trash(msg, botMsg));
+		if (!vc)
+			return msg.reply('Bot not in voice chat').then((botMsg) => trash(msg, botMsg));
+
+		// youtube stuff
+		let search = msg.content.slice(prefix.length).trim().split(/ +/).slice(1).join(' ').trim();
+
+		if (search.length === 0 || search === '')
+			return msg.reply('No search specified').then((botMsg) => trash(msg, botMsg));
+
+		let results, botMsg;
+
+		msg.channel.send(
+			new MessageEmbed()
+				.setTitle('Searching...'))
+			.then((mBotMsg) => botMsg = mBotMsg)
+			.then(() => YT.search.list({ q: search, part: 'snippet' }))
+			.then((result) => {
+				results = result.data.items;
+				let count = 0;
+				let embedText = results.map((result) => (count++, `**${count}**: \`[${result.snippet.channelTitle}] ${result.snippet.title}\`\n`));
+
+				// returns promise but doesn't resolve until user clicks delete so don't listen for promise
+				trash(msg, botMsg);
+
+				return botMsg.edit(new MessageEmbed().setTitle('Results').setDescription(embedText));
+			})
+			.then(() => Promise.all(emoji.map((e) => botMsg.react(e))))
+			.then(() => botMsg.awaitReactions((reaction, user) => emoji.includes(reaction.emoji.name) || reaction.emoji.name === '🗑️' && user.id === msg.author.id, { max: 1 }))
+			.then((collected) => results[emoji.indexOf(collected.first()['_emoji'].name)])
+			.then((video) => play(vc, ytdl(`https://www.youtube.com/watch?v=${video.id.videoId}`), msg))
+			.then(() => trash(msg, botMsg))
+	},
+
 	// vplay: (msg) => {
 
 	// },
 
-	// vpause: (msg) => {
+	vpause: (msg) => {
+		let vc = getVoice(msg);
 
-	// },
+		if (!isMemberVoice(msg))
+			return msg.reply('Please join a voice channel first!').then((botMsg) => trash(msg, botMsg));
+		if (!vc)
+			return msg.reply('Bot not in voice chat').then((botMsg) => trash(msg, botMsg));
+
+		// pause stuff
+		vc.dispatcher.pause();
+	},
+
+	vresume: (msg) => {
+		let vc = getVoice(msg);
+
+		if (!isMemberVoice(msg))
+			return msg.reply('Please join a voice channel first!').then((botMsg) => trash(msg, botMsg));
+		if (!vc)
+			return msg.reply('Bot not in voice chat').then((botMsg) => trash(msg, botMsg));
+
+		// pause stuff
+		vc.dispatcher.resume();
+	},
 
 	// vvup: (msg) => {
 
@@ -52,6 +120,36 @@ module.exports = {
 	// vvdown: (msg) => {
 
 	// }
+}
+
+function play(vc, item, msg) {
+	if (!vc.dispatcher) {
+		let dispatcher = vc.play(item, { quality: 'highestaudio' });
+		dispatcher.on('finish', () => queue[vc.channel.id].length > 0 && play(vc, queue[vc.channel.id].shift(), msg));
+
+		botMsg.channel.send(new MessageEmbed().setAuthor(`Now playing audio in ${vc.channel.name}`))
+			.then((mNewMsg) => newMsg = mNewMsg)
+			.then(() => botMsg.delete())
+			.then(() => newMsg.reactions.removeAll())
+			.then(() => newMsg.react('⏯'))
+			.then(() => {
+				let collector = newMsg.createReactionCollector((reaction, user) => reaction.emoji.name === '⏯' /*&& user.id === msg.author.id*/);
+				collector.on('collect', (r) => {
+					if (r.emoji.name === '⏯') {
+						vc.dispatcher.paused ? vc.dispatcher.resume() : vc.dispatcher.pause();
+						newMsg.reactions.resolve(r).users.remove(msg.member.id);
+					}
+				});
+			})
+			.then(() => trash(msg, botMsg))
+	} else {
+		if (!queue[vc.channel.id]) queue[vc.channel.id] = [];
+		queue[vc.channel.id].push(item);
+
+		botMsg.edit(new MessageEmbed().setAuthor(`Added to queue (queue length: ${queue[vc.channel.id].length})`))
+			.then(() => botMsg.reactions.removeAll())
+			.then(() => trash(msg, botMsg))
+	}
 }
 
 function isMemberVoice(msg) {
