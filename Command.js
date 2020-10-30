2bfcdb02dc2da213b45fc22586ef5e1a3e13e4b6
tycrek <josh.moore@jmoore.dev>
2020-10-30 08:02:06 -0600

    kinda broken rn

---
+++ Command.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const fs = require('fs-extra');
+const UsageEmbed = require('./UsageEmbed');
+
+class Command {
+	constructor(category, )
+}
+
+function execute(msg, args, config) {
+
+}
