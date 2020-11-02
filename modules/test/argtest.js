4413b5350f571f790e159eb40db2568555ecc462
tycrek <josh.moore@jmoore.dev>
2020-11-02 12:31:51 -0700

    max length variable test

--- modules/test/argtest.js
+++ modules/test/argtest.js
@@ -3,7 +3,7 @@ const Command = require('../../Command');
 class ArgTest extends Command {
 	execute(msg) {
 		const { args } = this.parseArgs(msg);
-		msg.channel.send(args[0].padEnd(args[1] || this.getVariable('length', msg.guild.id), args[0]))
+		msg.channel.send(args[0].padEnd(Math.min(args[1] || this.getVariable('length', msg.guild.id), this.getVariable('max', msg.guild.id)), args[0]))
 	}
 }
 
