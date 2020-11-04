3d5c500563d6cc9dd0593458eb79bd01b7ce0a1f
tycrek <josh.moore@jmoore.dev>
2020-11-04 13:02:49 -0700

    fixed schlong

--- modules/fun/schlong.js
+++ modules/fun/schlong.js
@@ -6,7 +6,7 @@ class SchlongCommand extends Command {
 		const { args } = this.parseArgs(msg);
 
 		let max = this.getVariable('max', msg.guild.id);
-		schlong = '8'.padEnd(Math.min(max, args[0]), '=') + 'D';
+		let schlong = args[0] < 0 ? '😳' : '8'.padEnd(Math.min(max, args[0]), '=') + 'D';
 
 		return msg.channel.send(schlong)
 			.then((botMsg) => this.trash(msg, botMsg))
