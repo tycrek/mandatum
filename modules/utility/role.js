713f5d5df01e9d44ee66e37779bfe1fadeecb9f7
tycrek <josh.moore@jmoore.dev>
2020-12-03 14:21:19 -0700

    rewrote role for better reply and case-insensitiveness

--- modules/utility/role.js
+++ modules/utility/role.js
@@ -5,10 +5,22 @@ class RoleCommand extends Command {
 	execute(msg) {
 		const { args } = this.parseArgs(msg);
 
+		if (args.length > 10)
+			return msg.reply('only do 10 roles at a time.').then((botMsg) => this.trash(msg, botMsg));
+
 		let roles = this.getConfig(msg.guild.id).settings.langroles.langroles;
-		return Promise.all([msg.member.roles.add(roles['⸻ LANGUAGES ⸻'])].concat(args.map((arg) =>
-			Object.keys(roles).includes(arg.replace('-', '')) && !arg.includes('LANGUAGES') ? (arg.startsWith('-') ? msg.member.roles.remove(roles[arg.replace('-', '')]) : msg.member.roles.add(roles[arg.replace('-', '')])) : '')))
-			.then(() => msg.channel.send('Done!'))
+		let rolesToAdd = [], rolesToRemove = [];
+
+		args.forEach((arg) => {
+			let roleId = roles[Object.keys(roles).find((role) => role.toLowerCase() === arg.replace('-', '').toLowerCase())];
+			roleId && (arg.startsWith('-') ? rolesToRemove : rolesToAdd).push(roleId);
+		});
+
+		return Promise.all([msg.member.roles.add(roles['⸻ LANGUAGES ⸻'])].concat(rolesToAdd.map((roleId) => msg.member.roles.add(roleId)), rolesToRemove.map((roleId) => msg.member.roles.remove(roleId))))
+			.then((_results) => msg.channel.send(
+				(rolesToAdd.length > 0 ? ('Roles added: ' + rolesToAdd.map((roleId) => `<@&${roleId}>`).join(', ')) : 'No roles added.') + '\n' +
+				(rolesToRemove.length > 0 ? ('Roles removed: ' + rolesToRemove.map((roleId) => `<@&${roleId}>`).join(', ')) : 'No roles removed.')
+			))
 			.then((botMsg) => this.trash(msg, botMsg));
 	}
 }
