46a20afb4f9b5112ef5f2c659de11c5864d61b5c
cosmicice <catcosmicice@protonmail.ch>
2021-05-21 17:05:05 +0400

    Use Crafatar instead of Minotar because Minotar is wack

--- modules/fun/mcskin.js
+++ modules/fun/mcskin.js
@@ -1,17 +1,20 @@
 const Command = require('../../Command');
 const { MessageEmbed } = require('discord.js');
+const fetch = require('node-fetch');
 
 class MCSkinCommand extends Command {
 	execute(msg) {
 		const { args } = this.parseArgs(msg);
-
-		return msg.channel.send(
-			new MessageEmbed()
-				.setTitle(`${args[0]}'s Minecraft skin`)
-				.setColor(0xFF4136)
-				.setImage(`https://minotar.net/armor/body/${args[0]}/150.png`)
-				.setFooter('https://minotar.net'))
-			.then((botMsg) => this.trash(msg, botMsg));
+		
+		return fetch(`https://api.mojang.com/users/profiles/minecraft/${args[0]}`)
+			.then(res => res.json())
+			.then(json => new MessageEmbed()
+			     .setTitle(`${args[0]}'s Minecraft skin`)
+			     .setColor(0xFF4136)
+			     .setImage(`https://crafatar.com/renders/body/${json.id}.png`)
+			     .setFooter('https://crafatar.com/'))
+			.then(embed => msg.channel.send(embed))
+			.then(botMsg => this.trash(msg, botMsg));
 	}
 }
 
